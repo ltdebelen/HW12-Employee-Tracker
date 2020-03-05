3ed6718b538fbd101625b8737c647cf30e535ecd
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-04 20:10:14 -0600

    Adding schema file

---
+++ sql/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employees_DB;
+CREATE DATABASE employees_DB;
+
+USE employees_DB;
+
+CREATE TABLE department( 
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary INT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);

8df57ee09d4183a059c877cba7c8857d3cd3c73e
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-04 21:05:03 -0600

    Added seeds for sql insertion and function for viewing all employees

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "L@wr3nc314",
-  database: "seinfeld"
+  database: "employees_DB"
 });
 
 connection.connect(function(err) {
@@ -15,5 +16,32 @@ connection.connect(function(err) {
 });
 
 function start() {
-  console.log("Working");
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View ALL Employees", "Add Employee", "Remove Employee"]
+    })
+    .then(response => {
+      switch (response.action) {
+        case "View ALL Employees":
+          viewAllEmployees();
+      }
+    });
+}
+
+function viewAllEmployees() {
+  const query = `
+  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+  FROM employee 
+  INNER JOIN role ON employee.id = role.id
+  INNER JOIN department ON role.department_id = department.id;
+  `;
+
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table("ALL EMPLOYEES", res);
+    start();
+  });
 }

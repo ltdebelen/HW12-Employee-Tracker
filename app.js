0f64e51a45f82a4d25105ea4b729c4719a53b489
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-07 23:27:25 -0600

    Fixing query to show user's manager

--- app.js
+++ app.js
@@ -60,10 +60,12 @@ function start() {
 
 function viewAllEmployees() {
   const query = `
-  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary
+  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
   FROM employee 
   INNER JOIN role ON employee.role_id = role.id
-  INNER JOIN department ON role.department_id = department.id;
+  INNER JOIN department ON role.department_id = department.id
+  LEFT JOIN employee AS manager ON manager.id = employee.manager_id
+  ORDER BY employee.id;
   `;
   connection.query(query, (err, res) => {
     if (err) throw err;

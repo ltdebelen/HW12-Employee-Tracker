204b45a86ee3ea1f00a9f2a96d3aa2de58662611
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-07 19:27:23 -0600

    Adding query to seed file and updating query for viewing all employees

--- app.js
+++ app.js
@@ -33,12 +33,11 @@ function start() {
 
 function viewAllEmployees() {
   const query = `
-  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+  SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary
   FROM employee 
-  INNER JOIN role ON employee.id = role.id
+  INNER JOIN role ON employee.role_id = role.id
   INNER JOIN department ON role.department_id = department.id;
   `;
-
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.table("ALL EMPLOYEES", res);
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -8,3 +8,6 @@ VALUES ("Sales Person", 10,000, 1)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Smith", 1, null);
